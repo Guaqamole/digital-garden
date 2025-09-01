5de4ed467c482277f2d8eda988aafcecb7a4bddd
Guaqamole <hiavokado@gmail.com>
2025-09-02 02:04:01 +0900

    Quarts Sync at: 2025-09-02 02:03:57 KST

--- sync.sh
+++ sync.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+
+# Get the current timestamp in KST
 timestamp=$(TZ='Asia/Seoul' date '+%Y-%m-%d %H:%M:%S')
 
-git pull
+# Git Pull
+git remote update
+git pull origin v4
 
+# Git commands
 git add .
-git commit -m "Quartz sync: $timestamp"
+git commit -m "Quarts Sync at: $timestamp KST"
 git push origin v4
